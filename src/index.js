4454ab047ca6e2a998f997faceca2480caad1a8a
John Grassey <johngrassey@gmail.com>
2024-08-19 19:23:15 -0400

    Create Projects and ToDos Modules
    Project module allows for adding and removing projects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 import "./styles.css";
+import { Projects } from "./projects";
+
---
+++ src/projects.js
@@ -0,0 +1,18 @@
+function Projects () {
+    const projectList = [];
+
+    const addProject = (project) => {
+        projectList.push(project);
+        console.log(projectList);
+    }
+
+    const delProject = (index) => {
+        projectList.splice(index, 1)
+        console.log(projectList);
+    }
+
+    return { addProject, delProject };
+
+};
+
+export { Projects }
